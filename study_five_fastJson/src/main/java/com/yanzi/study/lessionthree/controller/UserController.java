94d80964d7373e5335a6bcbb8c1bd0cdf624bffc
yanzi <296200192@qq.com>
2018-08-16 19:46:51 +0800

    8/16 SpringBoot整合FastJson

---
+++ study_five_fastJson/src/main/java/com/yanzi/study/lessionthree/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.yanzi.study.lessionthree.controller;
+
+import com.yanzi.study.lessionthree.entity.UserEntity;
+import com.yanzi.study.lessionthree.jpa.UserJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+*spring boot 整合druid
+*@author yanzi
+*@Date 14:10 2018/8/16
+**/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserJPA userJPA;
+
+    /**
+     * 查询用户列表方法
+     * @return
+     */
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public List<UserEntity> list(){
+        return userJPA.findAll();
+    }
+
+    /**
+     * 添加用户、更新用户方法
+     * @param entity
+     * @return
+     */
+    @RequestMapping(value = "/save",method = RequestMethod.GET)
+    public UserEntity save(UserEntity entity){
+        return userJPA.save(entity);
+    }
+
+    @RequestMapping(value = "/delete",method = RequestMethod.GET)
+    public List<UserEntity> delete(Long id){
+        userJPA.deleteById(id);
+        return userJPA.findAll();
+    }
+}
